bcc794285c7cf98b29b3023e00d3c8ed54783f97
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-03-12 19:11:03 +0200

    Fixed Compulsory3

--- WEEK3/exercises.sh
+++ WEEK3/exercises.sh
@@ -1,14 +1,15 @@
 #! /bin/sh 
 
 #1)
-cut -d : -f 1,7 /etc/passwd | grep "/sbin/nologin" | grep -E "^*[m-z]:" | head -n15 > max15.txt
+cut -d : -f 1,7 /etc/passwd | grep "/sbin/nologin" | grep -E "*[m-z]:" | head -n15 > max15.txt
 
 #2)
 ps -axo user,cmd,pid,tty | tr -s " " ":"| grep -e ":pts/[0,2,3,5]" | sort -t: -nk3 | grep ":bash:" > output.txt 
 
 #3)
-w -h | tr -s " " ":" | cut -d: -f3 | less -n8 > filtruIPs.txt
+
+who | tr -s " " | sort -k3, 4 -t" " | head -n8 | cut -d" " -f5 > filtruIPs.txt
 
 #4)
 
-ps -eo user,comm,pid --no-headers | tr -s "" | cut -d" " -f1,2,3 --output-delimiter=: | sort -k2 -t:
+ps -eo user,comm,pid --no-headers | tr -s " " | cut -d" " -f1,2,3 --output-delimiter=: | sort -k2 -t:
