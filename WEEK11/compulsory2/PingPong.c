4793f54544b2838606b147a219e47bfa162767cc
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-05-15 11:22:17 +0300

    WEEK 11 done

--- WEEK11/compulsory2/PingPong.c
+++ WEEK11/compulsory2/PingPong.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 
-void dialog_tata()
+void dialog_tata(char* token)
 {
    FILE *file = fopen("replica_tata.txt", "r");
 
@@ -16,48 +17,24 @@ void dialog_tata()
 
 
    char line[256];
-   int flag_fd;
-   char token = 0;
 
    while(fgets(line, sizeof(line), file))
    {
-    do{
-        flag_fd = open("flag.bin", O_RDONLY);
-
-        if(flag_fd==-1)
-        {
-            perror("open");
-            fclose(file);
-            return;
-        }
-
-        read(flag_fd, &token, 1);
-        close(flag_fd);
-        usleep(1000);
-    }while(token!=0);
-
-    printf("[P0] %s", line);
-    fflush(stdout);
-
-
-    token=1;
-    flag_fd = open("flag.bin", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-    if(flag_fd==-1)
+    while(*token!=0)
     {
-        perror("open");
-        fclose(file);
-        return;
+        usleep(1000);
     }
 
-    write(flag_fd, &token, 1);
-    close(flag_fd);
-   }
+    printf("[P0]: %s", line);
+    fflush(stdout);
 
+    *token=1;
+   }
+   printf("\n");
    fclose(file);
 }
 
-void dialog_fiu()
+void dialog_fiu(char *token)
 {
     FILE *file = fopen("replica_fiu.txt", "r");
 
@@ -69,63 +46,42 @@ void dialog_fiu()
 
 
    char line[256];
-   int flag_fd;
-   char token = 0;
+
 
    while(fgets(line, sizeof(line), file))
    {
-    do{
-        flag_fd = open("flag.bin", O_RDONLY);
-
-        if(flag_fd==-1)
-        {
-            perror("open");
-            fclose(file);
-            return;
-        }
-
-        read(flag_fd, &token, 1);
-        close(flag_fd);
-        usleep(1000);
-    }while(token!=1);
-
-    printf("[P1] %s", line);
-    fflush(stdout);
-
-
-    token=0;
-    flag_fd = open("flag.bin", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-    if(flag_fd==-1)
+    while(*token!=1)
     {
-        perror("open");
-        fclose(file);
-        return;
+        usleep(1000);
     }
 
-    write(flag_fd, &token, 1);
-    close(flag_fd);
-   }
+    printf("[P1]: %s", line);
+    fflush(stdout);
 
+    *token=0;
+   }
+   printf("\n");
    fclose(file);
 }
 
+
+
 int main()
 {
+
+    char *token = mmap(NULL, sizeof(char), PROT_READ|PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+    if(token==MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+
+    *token = 0;
+
     pid_t pid_fiu;
 
     // TODO: de inițializat cu 0 fișierul flag.bin
 
-    int fd = open("flag.bin", O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if(fd==-1)
-    {
-        perror("open");
-        exit(1);
-    }
-
-    char init_token=0;
-    write(fd, &init_token, 1);
-    close(fd);
 
     /* Crearea unui proces fiu. */
     if(-1 == (pid_fiu=fork()) )
@@ -137,20 +93,20 @@ int main()
     if(pid_fiu == 0)
     {   /* Zona de cod executată doar de către fiu. */
 
-        printf("\n[P1] Procesul fiu, cu PID-ul: %d.\n", getpid()); usleep(200);
+        //printf("\n[P1] Procesul fiu, cu PID-ul: %d.\n", getpid()); usleep(200);
 
-        dialog_fiu();
+        dialog_fiu(token);
     }
     else
     {   /* Zona de cod executată doar de către părinte. */
 
-        printf("\n[P0] Procesul tata, cu PID-ul: %d.\n", getpid()); usleep(200);
+        //printf("\n[P0] Procesul tata, cu PID-ul: %d.\n", getpid()); usleep(200);
 
-        dialog_tata();
+        dialog_tata(token);
     }
 
     /* Zona de cod comună, executată de către ambele procese */
-    printf("\nSfarsitul executiei procesului %s.\n\n", pid_fiu == 0 ? "fiu" : "parinte" );
-    unlink("flag.bin");
+    //printf("\nSfarsitul executiei procesului %s.\n\n", pid_fiu == 0 ? "fiu" : "parinte" );
+    munmap(token, sizeof(char));
     return 0;
 }
--- WEEK11/compulsory2/Supervisor-worker.c
+++ WEEK11/compulsory2/Supervisor-worker.c
@@ -3,60 +3,47 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 #include <sys/wait.h>
 
 #define SIZEOF_PAIRS 2*sizeof(int)
 
-typedef struct{
+typedef struct {
     int a;
     int b;
-}Pairs;
+    char op;
+}Operation;
 
 int N=0;
 
 char operatii[] = {'+', '-', '*', '/'};
 
-void calcul_tata()
+void calcul_tata(Operation *ops)
 {
-    Pairs p;
-    char op;
-    int result;
 
-    int fd1 = open("operanzi.bin", O_RDONLY);
-    if(fd1==-1)
+    for(int i=0; i<N; i++)
     {
-        perror("OPEN");
-        exit(1);
-    }
-    int fd2 = open("operators.txt", O_RDONLY);
-    if(fd2==-1)
-    {
-        perror("OPEN");
-        exit(1);
-    }
-
-    while(read(fd1, &p, SIZEOF_PAIRS)==SIZEOF_PAIRS && read(fd2, &op, 1)==1)
-    {
-        switch(op)
+        int result;
+        switch(ops[i].op)
         {
             case '+':
-                result = p.a + p.b;
+                result = ops[i].a + ops[i].b;
                 break;
             case '-':
-                result = p.a - p.b;
+                result = ops[i].a - ops[i].b;
                 break;
             case '*':
-                result = p.a * p.b;
+                result = ops[i].a * ops[i].b;
                 break;
             case '/':
-                if(p.b==0)
+                if(ops[i].b==0)
                 {
                     printf("ERROR: Division by zero!\n");
                     exit(1);
                 }
                 else
                 {
-                    result = p.a / p.b;
+                    result = ops[i].a / ops[i].b;
                 }
                 break;
             default:
@@ -64,52 +51,24 @@ void calcul_tata()
                 exit(1);
         }
 
-        printf("[P0] %d %c %d = %d\n", p.a, op, p.b, result);
+        printf("[P0] %d %c %d = %d\n", ops[i].a, ops[i].op, ops[i].b, result);
     }
 }
 
-void generare_fiu1()
+void generare_fiu1(Operation *ops)
 {
-    int fd = open("operanzi.bin", O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if(fd==-1)
-    {
-        perror("OPEN");
-        exit(1);
-    }
-
     for(int i=0; i<N; i++)
     {
-        Pairs p;
-        p.a=rand()%100 + 10;
-        p.b=rand()%100 + 10;
-
-        if(write(fd, &p, SIZEOF_PAIRS)!=SIZEOF_PAIRS)
-        {
-            perror("WRITE");
-            exit(1);
-        }
+        ops[i].a = rand()%100+10;
+        ops[i].b = rand()%100+10;
     }
-    close(fd);
 }
 
-void generare_fiu2()
+void generare_fiu2(Operation *ops)
 {
-    int fd = open("operators.txt", O_RDWR | O_CREAT | O_TRUNC, 0644);
-    if(fd==-1)
-    {
-        perror("OPEN");
-        exit(1);
-    }
-
     for(int i=0; i<N; i++)
     {
-        char op;
-        op=operatii[rand()%4];
-        if(write(fd, &op, 1)!=1)
-        {
-            perror("WRITE");
-            exit(1);
-        }
+        ops[i].op = operatii[rand()%4];
     }
 }
 
@@ -126,6 +85,15 @@ int main()
         exit(1);
     }
 
+    Operation *ops = mmap(NULL, N*sizeof(Operation), PROT_READ|PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+
+    if(ops==MAP_FAILED)
+    {
+        perror("mmap");
+        exit(1);
+    }
+
+
     /* Crearea procesului fiu #1. */
     if(-1 == (pid_fiu1=fork()) )
     {
@@ -137,7 +105,7 @@ int main()
     {   /* Zona de cod executată doar de către fiul #1. */
         printf("\n[P1] Procesul fiu 1, cu PID-ul: %d.\n", getpid());
 
-        generare_fiu1();
+        generare_fiu1(ops);
         return 0;
     }
     else
@@ -155,7 +123,7 @@ int main()
         {   /* Zona de cod executată doar de către fiul #2. */
             printf("\n[P2] Procesul fiu 2, cu PID-ul: %d.\n", getpid());
 
-            generare_fiu2();
+            generare_fiu2(ops);
             return 0;
         }
         else
@@ -165,7 +133,8 @@ int main()
             wait(NULL);
             // Mai întâi aștept terminarea ambilor fii, iar apoi fac calculul.
 
-            calcul_tata();
+            calcul_tata(ops);
+            munmap(ops, N*sizeof(Operation));
             return 0;
         }
     }
